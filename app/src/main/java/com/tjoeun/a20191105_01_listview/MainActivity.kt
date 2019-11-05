ff6c0dfa7eeab616db94021882de92a7baeac498
kiback2da <kiback2da@steconsulting.co.kr>
2019-11-05 20:31:27 +0900

    Room 데이터 클래스 추가   - 메인 Activity에서 데이터 추가를 별개 메소드로 작성   - sum 메소드로 코틀린만의 문법 확인

--- app/src/main/java/com/tjoeun/a20191105_01_listview/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191105_01_listview/MainActivity.kt
@@ -2,11 +2,52 @@ package com.tjoeun.a20191105_01_listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tjoeun.a20191105_01_listview.datas.RoomData
 
 class MainActivity : AppCompatActivity() {
+
+    val roomList = ArrayList<RoomData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addRooms()
+
+        sum(10,20)
+        sum2(10,20)
+
+    }
+
+    //Ex 두숫자의 합계를 Return 해주는 함수 생성
+    fun sum(num1:Int, num2:Int): Int {
+        val result = num1 + num2
+        return result
+    }
+
+    fun sum2(num1:Int, num2: Int) = num1 + num2
+
+    fun addRooms(){
+        //방을 추가 하는 로직
+        var room1 = RoomData()
+        room1.price = 8000
+        room1.address = "마포구 서교동"
+        room1.floor = 1
+
+        roomList.add(room1)
+
+        var room2 = RoomData()
+        room2.price = 28500
+        room2.address = "마포구 서교동"
+        room2.floor = 3
+
+        roomList.add(room2)
+
+        var room3 = RoomData()
+        room3.price = 12000
+        room3.address = "마포구 성산동"
+        room3.floor = 5
+
+        roomList.add(room3)
     }
 }
